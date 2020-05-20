e1a2a970bd544071a62b8e63274445925a8511d1
e.serqueira <eduardo.serqueira@outlook.com>
2020-05-20 16:33:45 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercicio_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double imposto;
+
+            Console.WriteLine("Digite o valor: ");
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (salario <= 2000.0)
+            {
+                imposto = 0.0;
+            }
+            else if (salario <= 3000.0)
+            {
+                imposto = (salario - 2000.0) * 0.08;
+            }
+            else if (salario <= 4500.0)
+            {
+                imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
+            }
+            else
+            {
+                imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+            }
+
+            if (imposto == 0.0)
+            {
+                Console.WriteLine("Isento");
+            }
+            else
+            {
+                Console.WriteLine("R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            //Exemplo colocar . nas casas decimais
+            //Entrada
+            //3002.00
+            //Saida
+            //80.36
+        }
+    }
+}
